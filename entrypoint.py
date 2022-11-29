03747782f3d1285894a26cee3041440891d4ad6a
csaskaa <110460477+csaskaa@users.noreply.github.com>
2022-11-29 15:27:58 +0100

    Extended upload plugin (#2)

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,7 @@
-from models.plugin import EmptyPlugin, PluginActionResponse, PluginExchangeMetadata
+from mescobrad_edge.plugins.edge_plugin_upload.models.plugin import EmptyPlugin, PluginActionResponse, PluginExchangeMetadata
 from io import BytesIO
 
-class UploadPlugin(EmptyPlugin):
+class GenericPlugin(EmptyPlugin):
 
     def action(self, input_meta: PluginExchangeMetadata = None) -> PluginActionResponse:
         import datetime
@@ -16,15 +16,27 @@ class UploadPlugin(EmptyPlugin):
                             config=Config(signature_version='s3v4'),
                             region_name=self.__OBJ_STORAGE_REGION__)
 
-        # Read incoming data
-        data = self.__load__(input_meta)
+        s3_local = boto3.resource('s3',
+                    endpoint_url= self.__OBJ_STORAGE_URL_LOCAL__,
+                    aws_access_key_id= self.__OBJ_STORAGE_ACCESS_ID_LOCAL__,
+                    aws_secret_access_key= self.__OBJ_STORAGE_ACCESS_SECRET_LOCAL__,
+                    config=Config(signature_version='s3v4'),
+                    region_name=self.__OBJ_STORAGE_REGION__)
 
-        # Upload data
-        obj_name = self.__OBJ_STORAGE_ARTEFACT_TEMPLATE__\
-            .replace('{ts}', str(datetime.datetime.now()))\
-            .replace('{fn}', input_meta.file_name)\
-            .replace('{fct}', input_meta.file_content_type)\
-            .replace('{co}', input_meta.created_on) # created_on
-        s3.Bucket(self.__OBJ_STORAGE_BUCKET__).upload_fileobj(BytesIO(data), obj_name, ExtraArgs={'ContentType': input_meta.file_content_type})
+
+        # Read incoming data and upload to cloud MinIO storage
+        for file in input_meta.file_name:
+            obj_name = self.__OBJ_STORAGE_ARTEFACT_TEMPLATE__.replace('{name}', file)
+            data = self.__load__(obj_name)
+            # Upload data
+            s3.Bucket(self.__OBJ_STORAGE_BUCKET__).upload_fileobj(BytesIO(data), obj_name, ExtraArgs={'ContentType': input_meta.file_content_type})
+
+
+        # Upload updated files to local MinIO storage
+        for file in input_meta.file_name:
+            data = self.__load__(file)
+            obj_name = self.__OBJ_STORAGE_ARTEFACT_TEMPLATE_LOCAL__.replace('{name}', file)
+            # Upload updated data
+            s3_local.Bucket(self.__OBJ_STORAGE_BUCKET__).upload_fileobj(BytesIO(data), obj_name, ExtraArgs={'ContentType': input_meta.file_content_type})
 
         return PluginActionResponse()
