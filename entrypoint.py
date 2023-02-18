05b1bed21380d0c88e0ae6f38c86a2499f22a286
csaskaa <110460477+csaskaa@users.noreply.github.com>
2023-02-18 17:02:18 +0100

    Delete files after processing and uploading (#4)

--- entrypoint.py
+++ entrypoint.py
@@ -4,9 +4,9 @@ from io import BytesIO
 class GenericPlugin(EmptyPlugin):
 
     def action(self, input_meta: PluginExchangeMetadata = None) -> PluginActionResponse:
-        import datetime
         import boto3
         from botocore.client import Config
+        import os
 
         # Init client
         s3 = boto3.resource('s3',
@@ -30,7 +30,8 @@ class GenericPlugin(EmptyPlugin):
             data = self.__load__(obj_name)
             # Upload data
             s3.Bucket(self.__OBJ_STORAGE_BUCKET__).upload_fileobj(BytesIO(data), obj_name, ExtraArgs={'ContentType': input_meta.file_content_type})
-
+            # Delete file after upload
+            os.remove("./" + obj_name)
 
         # Upload updated files to local MinIO storage
         for file in input_meta.file_name:
@@ -38,5 +39,6 @@ class GenericPlugin(EmptyPlugin):
             obj_name = self.__OBJ_STORAGE_ARTEFACT_TEMPLATE_LOCAL__.replace('{name}', file)
             # Upload updated data
             s3_local.Bucket(self.__OBJ_STORAGE_BUCKET_LOCAL__).upload_fileobj(BytesIO(data), obj_name, ExtraArgs={'ContentType': input_meta.file_content_type})
-
+            # Delete file after upload
+            os.remove("./" + file)
         return PluginActionResponse()
